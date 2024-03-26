a5364168ddf24d08513da0fb2595c68fc97e18fa
Taher Barakat <taher.barakat12223@gmail.com>
2024-03-26 18:53:59 +0300

    create Ingredient model

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { ingredient } from '../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrl: './shopping-list.component.css'
+  styleUrl: './shopping-list.component.css',
 })
 export class ShoppingListComponent {
-
+  ingredients: ingredient[] = [new ingredient('tomato', 3)];
 }
