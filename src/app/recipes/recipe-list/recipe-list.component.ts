59b577b62124d4d4d1a9959671cd729e451c857c
Taher Barakat <taher.barakat12223@gmail.com>
2024-04-11 14:29:52 +0300

    modify the recipe component to show infos about each recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from './recipe.model';
 @Component({
   selector: 'app-recipe-list',
@@ -6,12 +6,21 @@ import { Recipe } from './recipe.model';
   styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
       'Test Recipe',
       'this is a test',
       'https://t3.ftcdn.net/jpg/01/79/59/92/360_F_179599293_7mePKnajSM4bggDa8NkKpcAHKl3pow2l.webp'
     ),
+    new Recipe(
+      'Test Recipe2222',
+      'this is a test22222',
+      'https://t3.ftcdn.net/jpg/01/79/59/92/360_F_179599293_7mePKnajSM4bggDa8NkKpcAHKl3pow2l.webp'
+    ),
   ];
   constructor() {}
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
