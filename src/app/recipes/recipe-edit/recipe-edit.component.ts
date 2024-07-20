3754bbd5e2311befb34bf21698e489321a26bb7f
Taher Barakat <taher.barakat12223@gmail.com>
2024-07-20 18:08:28 +0300

    show the ingredients in recipe form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 
@@ -12,6 +12,7 @@ export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
   recipeForm: FormGroup;
+  ingredientArrayCtrls;
   constructor(
     private route: ActivatedRoute,
     private recipeService: RecipeService
@@ -28,21 +29,48 @@ export class RecipeEditComponent implements OnInit {
     let recipeName = '';
     let recipeImagePath = '';
     let recipeDescription = '';
-
+    let recipeIngredients = new FormArray([]);
     if (this.editMode) {
       const recipe = this.recipeService.getRecipe(this.id);
       recipeName = recipe.name;
       recipeImagePath = recipe.imagePath;
       recipeDescription = recipe.description;
+      if (recipe['ingredients']) {
+        for (let ingredient of recipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              name: new FormControl(ingredient.name),
+              amount: new FormControl(ingredient.amount),
+            })
+          );
+        }
+      }
     }
 
     this.recipeForm = new FormGroup({
       name: new FormControl(recipeName),
       imagePath: new FormControl(recipeImagePath),
       description: new FormControl(recipeDescription),
+      ingredients: recipeIngredients,
     });
+    this.ingredientArrayCtrls = (<FormArray>(
+      this.recipeForm.get('ingredients')
+    )).controls;
+    console.log((<FormArray>this.recipeForm.get('ingredients')).controls);
+    console.log(this.recipeForm.controls);
   }
   onSubmit() {
     console.log(this.recipeForm.value);
+    (<FormArray>this.recipeForm.get('ingredients')).controls.push(
+      new FormGroup({
+        name: new FormControl(),
+        amount: new FormControl(),
+      })
+    );
   }
+  onAddIngredient() {}
+  // log(x) {
+  //   console.log(x);
+  //   console.log('test');
+  // }
 }
